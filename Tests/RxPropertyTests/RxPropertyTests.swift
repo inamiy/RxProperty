f1218c60df72913c6993d81ef8e88874ae023ebc
Yasuhiro Inami <inamiy@gmail.com>
2019-01-30 15:32:59 +0900

    Reproduce `Property.init(unsafeObservable:)` bug that doesn't emit value when `property` is deallocated

--- Tests/RxPropertyTests/RxPropertyTests.swift
+++ Tests/RxPropertyTests/RxPropertyTests.swift
@@ -84,6 +84,84 @@ final class RxPropertyTests: XCTestCase {
 
     }
 
+    // MARK: - initWithUnsafe
+
+    func test_initWithUnsafe_value() {
+
+        let relay = BehaviorRelay<Int>(value: 0)
+        let property = Property<Int>(unsafeObservable: relay.asObservable())
+
+        XCTAssertEqual(property.value, 0)
+
+        relay.accept(1)
+        XCTAssertEqual(property.value, 1)
+
+        relay.accept(2)
+        XCTAssertEqual(property.value, 2)
+
+    }
+
+    func test_initWithUnsafe_asObservable() {
+
+        let relay = BehaviorRelay<Int>(value: 0)
+        var property: Property<Int>! = .init(unsafeObservable: relay.asObservable())
+
+        var events = [Event<Int>]()
+
+        // `.asObservable()` test
+        _ = property.asObservable().subscribe { event in
+            events.append(event)
+        }
+
+        XCTAssertEqual(events, [.next(0)],
+                       "should observe initial value")
+
+        relay.accept(1)
+        XCTAssertEqual(events, [.next(0), .next(1)])
+
+        relay.accept(2)
+        XCTAssertEqual(events, [.next(0), .next(1), .next(2)])
+
+        property = nil
+        XCTAssertEqual(events, [.next(0), .next(1), .next(2)],
+                       "`.completed` should NOT be observed when `property` is deallocated")
+
+        relay.accept(3)
+        XCTAssertEqual(events, [Event<Int>.next(0), .next(1), .next(2), .next(3)],
+                       "`property`'s observable should still be alive even when `property` is deallocated.")
+
+    }
+
+    func test_initWithUnsafe_changed() {
+
+        let relay = BehaviorRelay<Int>(value: 0)
+        var property: Property<Int>! = .init(unsafeObservable: relay.asObservable())
+
+        var events = [Event<Int>]()
+
+        // `.changed` test
+        _ = property.changed.subscribe { event in
+            events.append(event)
+        }
+
+        XCTAssertEqual(events, [Event<Int>](),
+                       "should NOT observe initial value")
+
+        relay.accept(1)
+        XCTAssertEqual(events, [.next(1)])
+
+        relay.accept(2)
+        XCTAssertEqual(events, [.next(1), .next(2)])
+
+        property = nil
+        XCTAssertEqual(events, [.next(1), .next(2)])
+
+        relay.accept(3)
+        XCTAssertEqual(events, [Event<Int>.next(1), .next(2), .next(3)],
+                       "`property`'s observable should still be alive even when `property` is deallocated.")
+
+    }
+
 }
 
 // MARK: RxSwift.Event + Equatable
